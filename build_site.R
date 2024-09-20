6e2a6534a18ff3ed2149a365b27c38ed9a337b71
armando <dsaavedra1012@gmail.com>
2024-09-19 23:27:41 -0400

    I added more exercises and redifine the sql blog

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("C:/Users/dsaav/Documents/Projects/R/Portfolio/Project 2")
+setwd("C:/Users/dsaav/Documents/Projects/R/Portfolio/Project 5")
 
 #render your sweet site. 
 rmarkdown::render_site()
